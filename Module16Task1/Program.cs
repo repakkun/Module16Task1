ec67cdeee72ce93362d38843c1bed040d59652e0
rePakkun <cj.ilya@yandex.ru>
2021-01-08 19:42:42 +0300

    Добавьте файлы проекта.

---
+++ Module16Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module16Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {               
+                          
+                Console.WriteLine("Введите путь до папки");
+                string folderName = Console.ReadLine();
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderName);
+                if (directoryInfo.Exists)
+                {
+                    var start = DateTime.Now;   
+                    var end = directoryInfo.LastWriteTime;
+                    if (start - end >= TimeSpan.FromMinutes(30))
+                    {
+                        directoryInfo.Delete(true);
+                        Console.WriteLine("Папка удалена");
+                    }
+                    else { Console.WriteLine("Ничего не удалено"); }
+                }          
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
